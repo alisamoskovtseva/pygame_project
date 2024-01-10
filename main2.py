f24280ae9c2a55d05e80297470e21430bb73c0aa
alice <143882734+alisamoskovtseva@users.noreply.github.com>
2024-01-10 19:37:03 +0300

    составление готовой судоку

--- main2.py
+++ main2.py
@@ -1,11 +1,12 @@
 import random
 
-
-hor = [set(range(10)), set(range(10)), set(range(10)), set(range(10)), set(range(10)), set(range(10)), set(range(10)), set(range(10)), set(range(10))]
-ver = [set(range(10)), set(range(10)), set(range(10)), set(range(10)), set(range(10)), set(range(10)), set(range(10)), set(range(10)), set(range(10))]
-kvadr = [[set(range(10)), set(range(10)), set(range(10))],
-         [set(range(10)), set(range(10)), set(range(10))],
-         [set(range(10)), set(range(10)), set(range(10))]]
+hor = [set(range(1, 10)), set(range(1, 10)), set(range(1, 10)), set(range(1, 10)), set(range(1, 10)), set(range(1, 10)),
+       set(range(1, 10)), set(range(1, 10)), set(range(1, 10))]
+ver = [set(range(1, 10)), set(range(1, 10)), set(range(1, 10)), set(range(1, 10)), set(range(1, 10)), set(range(1, 10)),
+       set(range(1, 10)), set(range(1, 10)), set(range(1, 10))]
+kvadr = [[set(range(1, 10)), set(range(1, 10)), set(range(1, 10))],
+         [set(range(1, 10)), set(range(1, 10)), set(range(1, 10))],
+         [set(range(1, 10)), set(range(1, 10)), set(range(1, 10))]]
 
 sud = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
        [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -26,7 +27,7 @@ for i in range(9):
             sud[i][j] = r
             hor[i].remove(r)
             ver[j].remove(r)
-            kvadr[i//3][j//3].remove(r)
+            kvadr[i // 3][j // 3].remove(r)
 print(sud)
 # [[7, 9, 5, 8, 0, 2, 1, 6, 4],
 #  [8, 4, 3, 6, 5, 7, 2, 9, 0],
@@ -37,4 +38,3 @@ print(sud)
 #  [0, 1, 2, 3, 7, 6, 4, 8, 5],
 #  [4, 7, 9, 0, 8, 5, 3, 0, 2],
 #  [3, 5, 6, 9, 1, 4, 0, 7, 0]]
-
